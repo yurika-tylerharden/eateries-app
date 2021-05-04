fb74aa60ab64b29ab107ea2abb2c2d32b6fa7f7f
Tyler Harden <tyler-harden@outlook.com>
2021-05-04 13:54:40 +1000

    Initial Commit

---
+++ Eateries/EateriesApp.swift
@@ -0,0 +1,17 @@
+//
+//  EateriesApp.swift
+//  Eateries
+//
+//  Created by Tyler Harden on 4/5/21.
+//
+
+import SwiftUI
+
+@main
+struct EateriesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
