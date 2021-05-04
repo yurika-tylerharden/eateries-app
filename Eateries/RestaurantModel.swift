e938d843e89c1a2766d7a49d9944801fe1a6a129
Tyler Harden <tyler-harden@outlook.com>
2021-05-04 14:03:54 +1000

    Created model for Restaurant

---
+++ Eateries/RestaurantModel.swift
@@ -0,0 +1,37 @@
+//
+//  RestaurantModel.swift
+//  Eateries
+//
+//  Created by Tyler Harden on 4/5/21.
+//
+
+import Foundation
+import SwiftUI
+import Combine
+import UIKit
+
+struct Restaurant: Hashable, Codable, Identifiable   {
+    public var id = UUID()
+    
+    var name: String
+    var location: String
+    var notes: String
+    var image: String
+    var reviews: Array<String>
+
+    //will return UIImage from a URL string
+   
+    
+}
+
+extension Binding where Value: MutableCollection, Value.Element: Identifiable {
+    /// Subscript for a given, identifiable element
+    subscript(identifiedBy element: Value.Element) -> Binding<Value.Element> {
+        return Binding<Value.Element>(get: {
+            return wrappedValue.first { $0.id == element.id } ?? element
+        }, set: { newValue in
+            guard let i = wrappedValue.firstIndex(where: { $0.id == element.id }) else { return }
+            wrappedValue[i] = newValue
+        })
+    }
+}
